697fea9c4769a1a0b08876c22d19b9cfc0ed0ae1
Juan Grasso <grasso.jm@gmail.com>
2023-04-22 08:43:16 +0200

    PreEntrega2Grasso

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,46 +1,49 @@
-import React from 'react';
-import CartWidget from '../CartWidget/CartWidget.jsx';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from '../CartWidget/CartWidget';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = () => {
+function NavBar() {
   return (
-    <header id='top'>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <a className="navbar-brand">Ticket2Bike</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Ofertas</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Categorías
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Mountain Bike</a></li>
-                  <li><a className="dropdown-item" href="#">Ruta</a></li>
-                  <li><a className="dropdown-item" href="#">Gravel</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Repuestos</a></li>
-                  <li><a className="dropdown-item" href="#">Accesorios</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Dale!</button>
-              <div id='cartButton'><CartWidget /></div>
-            </form>
-          </div>
-        </div>
-      </nav>
+    <header>
+      <Navbar bg="none" expand="lg">
+        <Container fluid>
+          <Navbar.Brand as={Link} to={"/"}>Ticket 2 Bike</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarButton" />
+          <Navbar.Collapse id="navbarButton">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+            >
+              
+              {/* <Nav.Link as={Link} to={"/on-sale"}>Ofertas</Nav.Link> */}
+              <NavDropdown title="Categorías" id="navbarScrollingDropdown">
+                <NavDropdown.Item as={Link} to={"/category/1"}>Mountain Bike</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/category/2"}>Ruta</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/category/3"}>Gravel - CX</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to={"/category/4"}>Repuestos</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/category/5"}>Accesorios</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Buscar"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Dale!</Button>
+            </Form>
+          </Navbar.Collapse>
+          <div id='cartButton'><CartWidget CartCount={4}/></div>
+        </Container>
+      </Navbar>
     </header>
   );
 }
